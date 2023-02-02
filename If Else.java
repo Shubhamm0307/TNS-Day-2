ca4010052994ddfe0b5f913d093ac180c8fa0aa9
shubhamm0307 <124040069+shubhamm0307@users.noreply.github.com>
2023-02-02 15:41:39 +0530

    Create If Else.java

---
+++ If Else.java
@@ -0,0 +1,15 @@
+package second;
+
+public class ifelse {
+
+	public static void main(String[] args) {
+		int age=11;
+		
+		if(age>=18)
+			System.out.println("You are eligible for voting");
+		else
+			System.out.println("Sorry!You need to wait more");
+		System.out.println("if lse Demonstration");
+	}
+
+}
